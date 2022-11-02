a2fae7fc408e56cd3d10cc182d8a2586925c8eb4
Fernando Alves <fernandu.alves@gmail.com>
2022-11-02 07:07:09 -0300

    added menu reducer to store

--- front/src/store.js
+++ front/src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./features/cart/cartSlice";
 import userReducer from "./features/user/userSlice";
+import menuReducer from "./features/menu/menuSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     user: userReducer,
+    menu: menuReducer,
   },
 });
