83546a5d20401798bbd8cc55e778007bc1003a8b
Fernando Alves <fernandu.alves@gmail.com>
2022-11-02 07:11:40 -0300

    refactored to use data from database

--- front/src/components/Dish.js
+++ front/src/components/Dish.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setCurrentItem } from "../features/menu/menuSlice";
 
-const Dish = ({ name, picture, price, id }) => {
+const Dish = ({ title, picture, price, _id }) => {
+  const dispatch = useDispatch();
+
   return (
     <article className="dish">
-      <Link to={`/products/${id}`}>
-        <img className="dish-img" src={picture} alt={name} />
-        <p className="dish-name">{name}</p>
-        <p className="dish-price">$ {price}</p>
+      <Link to={`/products/${_id}`}>
+        <img className="dish-img" src={picture} alt={title} />
+        <p className="dish-name">{title}</p>
+        <p className="dish-price">R$ {price.toFixed(2)}</p>
       </Link>
     </article>
   );
