7f183a1058f6a9bce2df3b9adb60f8bbd9d36613
Fernando Alves <fernandu.alves@gmail.com>
2022-11-02 07:10:08 -0300

    useefect with async reducer to retrieve menu items

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -5,33 +5,45 @@ import { dishes } from "../data";
 import Dish from "../components/Dish";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { getMenuItems, test } from "../features/menu/menuSlice";
 
 const MainPage = () => {
+  const { menuItems } = useSelector((store) => store.menu);
+  const dispatch = useDispatch();
+
+  const url = "http://localhost:5000/menu/all";
+
   useEffect(() => {
-    const auth = getAuth();
-    getRedirectResult(auth)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access Google APIs.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
+    dispatch(getMenuItems());
   }, []);
+
+  // useEffect(() => {
+  //   const auth = getAuth();
+  //   getRedirectResult(auth)
+  //     .then((result) => {
+  //       // This gives you a Google Access Token. You can use it to access Google APIs.
+  //       const credential = GoogleAuthProvider.credentialFromResult(result);
+  //       const token = credential.accessToken;
+
+  //       // The signed-in user info.
+  //       const user = result.user;
+  //       console.log(user);
+  //     })
+  //     .catch((error) => {
+  //       // Handle Errors here.
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+
+  //       // The AuthCredential type that was used.
+  //       const credential = GoogleAuthProvider.credentialFromError(error);
+  //       // ...
+  //     });
+  // }, []);
 
   const { amount } = useSelector((store) => store.cart);
   return (
@@ -50,8 +62,8 @@ const MainPage = () => {
       <h3 className="title-main">Comidas deliciosas pra você!</h3>
       <Categories />
       <div className="dishes-container">
-        {dishes.map((dish) => {
-          return <Dish key={dish.id} {...dish} />;
+        {menuItems.map((dish) => {
+          return <Dish key={dish._id} {...dish} />;
         })}
       </div>
       <Footer />
