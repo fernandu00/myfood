c28636e6098e80031a3a1235427bffdeddde8925
Fernando Alves <fernandu.alves@gmail.com>
2022-11-08 15:44:24 -0300

    dynamic categories menu based on menu categories from menu items

--- front/src/components/Categories.js
+++ front/src/components/Categories.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { categories } from "../data";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { RiContactsBookLine } from "react-icons/ri";
+import { useSelector } from "react-redux";
+
 const Categories = () => {
+  const { menuItems } = useSelector((store) => store.menu);
+  const [active, setActive] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = () => {
+    const cat = menuItems.map((item) => item.category);
+    setCategories([...new Set(cat)]);
+  };
+
+  const handleCategory = (name) => {
+    setActive(true);
+    menuItems.filter((item) => item.category === name);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <div className="categories-list">
       {categories.map((cat, index) => {
         return (
-          <article className="cat-item" key={index}>
+          <article
+            key={index}
+            className={active ? "cat-item active" : "cat-item"}
+            onClick={() => handleCategory(cat)}
+          >
             {cat}
           </article>
         );
