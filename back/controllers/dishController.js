98f40fa1903edd66f4177ef2e60e479b2f12d199
Fernando Alves <fernandu.alves@gmail.com>
2022-11-08 15:42:12 -0300

    delete by id function

--- back/controllers/dishController.js
+++ back/controllers/dishController.js
@@ -1,10 +1,12 @@
 const dishModel = require("../models/dishModel");
 
+// lists all menu items
 const getItems = async (req, res) => {
   const items = await dishModel.find({});
   return res.status(201).json(items);
 };
 
+// create a menu item
 const createDish = async (req, res) => {
   const { title, desc, category, price, picture } = req.body;
   if (!title || !desc || !category || !price || !picture) {
@@ -21,6 +23,7 @@ const createDish = async (req, res) => {
   }
 };
 
+// update menu item based on id
 const updateDish = async (req, res) => {
   try {
     const dish = await dishModel.findById(req.params.id);
@@ -41,4 +44,15 @@ const updateDish = async (req, res) => {
   }
 };
 
-module.exports = { getItems, createDish, updateDish };
+// delete menu item based on id
+const deleteDish = async (req, res) => {
+  dishId = req.params.id;
+  try {
+    const dishToDelete = await dishModel.findByIdAndDelete(dishId);
+    return res.status(201).json({ success: true, msg: dishToDelete });
+  } catch (error) {
+    return res.status(500).json({ success: false, msg: error });
+  }
+};
+
+module.exports = { getItems, createDish, updateDish, deleteDish };
