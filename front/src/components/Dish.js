9ef68f0a081c9074afdc2a97313d42883a6448cf
Fernando Alves <fernandu.alves@gmail.com>
2022-11-09 19:24:19 -0300

    minor changes in variable names from amount to quantity

--- front/src/components/Dish.js
+++ front/src/components/Dish.js
@@ -3,15 +3,13 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { setCurrentItem } from "../features/menu/menuSlice";
 
-const Dish = ({ title, picture, price, _id }) => {
-  const dispatch = useDispatch();
-
+const Dish = ({ title, picture, unit_price, _id }) => {
   return (
     <article className="dish">
       <Link to={`/products/${_id}`}>
         <img className="dish-img" src={picture} alt={title} />
         <p className="dish-name">{title}</p>
-        <p className="dish-price">R$ {price.toFixed(2)}</p>
+        <p className="dish-price">R$ {unit_price.toFixed(2)}</p>
       </Link>
     </article>
   );
