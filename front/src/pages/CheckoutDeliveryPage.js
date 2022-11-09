369ebac793e72fedc9dde34af4f2a3e14b5d1424
Fernando Alves <fernandu.alves@gmail.com>
2022-11-09 19:25:28 -0300

    create order function created to integrate payment gateway

--- front/src/pages/CheckoutDeliveryPage.js
+++ front/src/pages/CheckoutDeliveryPage.js
@@ -1,9 +1,39 @@
 import React from "react";
 import { FaChevronLeft } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Delivery from "../components/Delivery";
+import { useSelector } from "react-redux";
+import PaymentChoice from "../components/PaymentChoice";
+import axios from "axios";
 
 const CheckoutDeliveryPage = () => {
+  const { total, quantity, paymentOption, cartItems } = useSelector(
+    (store) => store.cart
+  );
+  const navigate = useNavigate();
+  const { uuid } = useSelector((store) => store.user);
+  // const url = "http://localhost:5000/order/new";
+
+  const url = "http://localhost:5000/payment";
+
+  const createOrder = async (e) => {
+    e.preventDefault();
+    const order = {
+      uuid,
+      quantity,
+      paymentOption,
+      items: cartItems,
+      total,
+    };
+    try {
+      const resp = await axios.post(url, order);
+      console.log(resp);
+      window.location.replace(resp.data.init_point);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className="delivery-main">
       <div className="cart-bar-container">
@@ -13,11 +43,14 @@ const CheckoutDeliveryPage = () => {
         <h3>Pedido</h3>
       </div>
       <Delivery />
+      <PaymentChoice />
       <div className="totals checkout-totals">
         <span>total</span>
-        <span>99.99</span>
+        <span>{total.toFixed(2)}</span>
       </div>
-      <button className="btn">Pagar</button>
+      <button className="btn" onClick={createOrder}>
+        Pagar
+      </button>
     </main>
   );
 };
