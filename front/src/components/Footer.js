6e13dced4557601f0a7d68c3f73367e7abc10a9b
Fernando Alves <fernandu.alves@gmail.com>
2022-11-17 09:54:30 -0300

    conditional render based on admin role added

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,19 +1,43 @@
 import React from "react";
-import { FaHome, FaRegHeart, FaRegUser } from "react-icons/fa";
+import { FaHome, FaRegHeart, FaRegUser, FaMotorcycle } from "react-icons/fa";
 import { BsClockHistory } from "react-icons/bs";
 import { RiHistoryFill } from "react-icons/ri";
 import { BiUser } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Footer = () => {
+  const { isAdmin } = useSelector((store) => store.user);
   return (
     <footer className="footer">
-      <FaHome className="function-icon footer-item" />
+      {isAdmin ? (
+        <Link to="/admin/main">
+          <FaHome className="function-icon footer-item" />
+        </Link>
+      ) : (
+        <Link to="/main">
+          <FaHome className="function-icon footer-item" />
+        </Link>
+      )}
 
-      <Link to="/profile">
-        <BiUser className="function-icon footer-item" />
-      </Link>
-      <RiHistoryFill className="function-icon footer-item" />
+      {isAdmin ? (
+        <Link to="/admin/togo">
+          <FaMotorcycle className="function-icon footer-item" />
+        </Link>
+      ) : (
+        <Link to="/profile">
+          <BiUser className="function-icon footer-item" />
+        </Link>
+      )}
+      {isAdmin ? (
+        <Link to="/admin/orderhistory">
+          <RiHistoryFill className="function-icon footer-item" />
+        </Link>
+      ) : (
+        <Link to="/history">
+          <RiHistoryFill className="function-icon footer-item" />
+        </Link>
+      )}
     </footer>
   );
 };
